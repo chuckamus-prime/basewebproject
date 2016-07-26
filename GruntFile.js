0175193b96ba131ad289b86827aa7b87f9eefb6c
Chuck Sailors <e18d1351@opayq.com>
2016-07-26 10:38:49 -0400

    in preparation for migrating our changes for our other poc that has angular 2 working with tests, we cleaned up this project
    it now only has angular 2 as a node dependency (+ whats required) and no
    source files. compiles (since there's nothing to compile!)

--- GruntFile.js
+++ GruntFile.js
@@ -2,76 +2,36 @@ module.exports = function (grunt) {
 	"use strict";
 	grunt.initConfig ({
 		ts : {
-			options: {
-				compiler: './node_modules/typescript/bin/tsc'
+			options : {
+				compiler : './node_modules/typescript/bin/tsc'
 				//note, this relies on the package.json file to specify the typescript version the project is using.
 			},
 			default : {
-				src: ['src/**/*.ts'],
-				"options": {
-					"module": "system",
-					"target": "ES5",
-					"moduleResolution": "node",
-					"emitDecoratorMetadata": true,
-					"noImplicitAny": false,
-					"removeComments": true,
-					"sourceMap": true
+				src : ['src/**/*.ts'],
+				"options" : {
+					"module" : "system",
+					"target" : "ES5",
+					"moduleResolution" : "node",
+					"emitDecoratorMetadata" : true,
+					"noImplicitAny" : false,
+					"removeComments" : true,
+					"sourceMap" : true
 				}
 			}
 		},
-		less: {
-			default: {
-				options: {
-					paths: ['src/content/**']
+		less : {
+			default : {
+				options : {
+					paths : ['src/content/**']
 				},
-				files:[{src: ['src/content/Site.less'], dest: 'src/content/Site.css'}]
+				files : [{src : ['src/content/Site.less'], dest : 'src/content/Site.css'}]
 			}
-		},
-		karma: {
-			unit: {
-//				basePath: '.',
-				logLevel:"debug",
-				frameworks: ['jasmine'],
-				singleRun: true,
-				browsers: ['PhantomJS'],
-				plugins: ['karma-jasmine','karma-phantomjs-launcher'],
-				options: {
-					files: [
-						// paths loaded by Karma
-						{pattern: 'node_modules/es6-shim/es6-shim.min.js', included: true, watched: true},
-						{pattern: 'node_modules/reflect-metadata/Reflect.js', included: true, watched: true},
-						{pattern: 'node_modules/zone.js/dist/zone.js', included: true, watched: true},
-						{pattern: 'node_modules/zone.js/dist/async-test.js', included: true, watched: true},
-						{pattern: 'node_modules/systemjs/dist/system-polyfills.js', included: true, watched: true},
-						{pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
-						{pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false},
-						{pattern: 'node_modules/@angular/**/*.js', included: false, watched: false},
-						{pattern: 'karma-test-shim.js', included: true, watched: true},
-
-						// Paths loaded via module imports
-						{pattern: 'src/app/Components/*Spec.js', included: true, watched: true},
-
-						// Paths to support debugging with source maps in dev tools
-						{pattern: 'src/app/Components/*.ts', included: false, watched: true},
-						{pattern: 'src/app/Components/*.js.map', included: false, watched: false}
-					]
-// 					proxies: {
-// 						// required for component assests fetched by Angular's compiler
-// 						'/src/app': '/base/src/app'
-// 					}
-// 					files: [
-// 						'src/scripts/angular/angular.js',
-// 						'node_modules/angular-mocks/angular-mocks.js',
-// 						'target/app/**/*.js',
-// 						'src/test/*tests.js'
-// 					]
-				}
-			}
-
-
 		}
 	});
 	grunt.loadNpmTasks ('grunt-ts');
+	grunt.loadNpmTasks ('grunt-contrib-less');
+	//grunt.loadNpmTasks('grunt-karma');
+
 	grunt.registerTask ('default', ['ts:default', 'less:default']);
 	grunt.registerTask ('compile', ['ts:default', 'less:default']);
 	//grunt.registerTask ('test', ['karma:unit']);
@@ -81,6 +41,4 @@ module.exports = function (grunt) {
 		grunt.log.write ('Logging some stuff...').ok ();
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-less');
-	//grunt.loadNpmTasks('grunt-karma');
 };
--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import {QuoteComponent} from './Components/QuoteComponent'
-
-@Component({
-	selector: 'my-app',
-	template: '<h1>My First Angular 2 App</h1><my-quote></my-quote>'
-})
-
-export class AppComponent { }
