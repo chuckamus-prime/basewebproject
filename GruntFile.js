1e2c15618b25b8ad522e99fa2ac64de4112a4157
Chuck Sailors <e18d1351@opayq.com>
2016-07-26 12:31:13 -0400

    updated the packages, moved typings into the grunt file. added a sanity check test. it builds and runs tests.

--- GruntFile.js
+++ GruntFile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
 				//note, this relies on the package.json file to specify the typescript version the project is using.
 			},
 			default : {
-				src : ['src/**/*.ts'],
+				src : ['./typings/globals/*/*.d.ts','src/**/*.ts'],
 				"options" : {
 					"module" : "system",
 					"target" : "ES5",
@@ -17,6 +17,35 @@ module.exports = function (grunt) {
 					"removeComments" : true,
 					"sourceMap" : true
 				}
+			}
+		},
+		typings: {
+			install: {
+				"globalDependencies": {
+				"es6-shim": "github:DefinitelyTyped/DefinitelyTyped/es6-shim/es6-shim.d.ts#6697d6f7dadbf5773cb40ecda35a76027e0783b2"
+				},
+				"globalDevDependencies":{
+					"jasmine": "github:DefinitelyTyped/DefinitelyTyped/jasmine/jasmine.d.ts#26c98c8a9530c44f8c801ccc3b2057e2101187ee"
+				}}
+		},
+		karma:{
+			unit: {
+				//configFile: 'karma.conf.js'
+
+				basePath: '',
+
+				frameworks: ['jasmine'],
+
+				logLevel: 'debug',
+
+				files: [{src:'src/**/*.js'}],
+
+				reporters: ['progress'],
+				port: 9876,
+				colors: true,
+				autoWatch: true,
+				browsers: ['PhantomJS'],
+				singleRun: true
 			}
 		},
 		less : {
@@ -29,12 +58,13 @@ module.exports = function (grunt) {
 		}
 	});
 	grunt.loadNpmTasks ('grunt-ts');
+	grunt.loadNpmTasks('grunt-typings');
 	grunt.loadNpmTasks ('grunt-contrib-less');
-	//grunt.loadNpmTasks('grunt-karma');
+	grunt.loadNpmTasks('grunt-karma');
 
 	grunt.registerTask ('default', ['ts:default', 'less:default']);
-	grunt.registerTask ('compile', ['ts:default', 'less:default']);
-	//grunt.registerTask ('test', ['karma:unit']);
+	grunt.registerTask ('compile', ['typings:install','ts:default', 'less:default']);
+	grunt.registerTask ('test', ['karma:unit']);
 	//We need to add Jasmine to the test task above
 	grunt.registerTask ('deploy', ['ts:default', 'less:default']);
 	grunt.registerTask ('default', 'Log some stuff.', function () {
